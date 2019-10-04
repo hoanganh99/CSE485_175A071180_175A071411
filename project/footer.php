f7adfff792eebc9c79266221e277fee7af6c0dc3
hoanhanh <anhhtn72@wru.vn>
2019-10-04 09:07:58 +0700

    BTL
    trang chủ

---
+++ project/footer.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<meta charset="UTF-8">
+    <meta name="description" content="Đại học Nguyễn Tất Thành">
+    <meta name="keywords" content="NTTU, Đại học Nguyễn Tất Thành">
+    <meta name="author" content="Hoàng Ánh, Huyền Trang">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Trường Đại Học Nguyễn Tất Thành</title>
+	<!--boostrap 4-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<!--css-->
+	<link rel="stylesheet" media="screen" type="text/css" href="css/style.css">
+	<!--icon-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+	<!--phần tại sao chọn NTTU-->
+	<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+	<!--javascript-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+	</script>
+	<!--jquery-->
+	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+	<div class="footer">
+		<div class="contact">
+			<div class="wrapper">
+				<div class="row ">
+					<div class="col-md-6 col-lg-3">
+						<h4><a href="">ĐH NGUYỄN TẤT THÀNH</a></h4>
+							<ul>
+								<li><a href="">Thư ngỏ</a></li>
+								<li><a href="">Tầm nhìn - Sứ mạng</a></li>
+								<li><a href="">Văn hóa ĐH Nguyễn Tất Thành</a></li>
+								<li><a href="">Thông tin 3 công khai</a></li>
+								<li><a href="">Đảm bảo chất lượng</a></li>
+								<li><a href="">Phát triển bền vững</a></li>
+								<li><a href="">Trường Trung cấp Nguyễn Tất Thành</a></li>
+								<li><a href="">Trường Tiểu học Anh Việt Mỹ</a></li>
+							</ul>
+						</div>
+						<div class="col-md-6 col-lg-3 cot-2">
+							<h4><a href="">PHÒNG - BAN</a></h4>
+							<h4><a href="">THƯ VIỆN ẢNH</a></h4>
+							<h4><a href="">E-LEARNING</a></h4>
+						</div>
+						<div class="col-md-6 col-lg-3">
+							<h4><a href="">TRUNG TÂM - VIỆN</a></h4>
+							<ul>
+								<li><a href="">Viện Đào Tạo Quốc Tế</a></li>
+								<li><a href="">Viện Công Nghệ Cao</a></li>
+								<li><a href="">Viện Đào tạo Sau đại học</a></li>
+								<li><a href="">Trung tâm Thông tin Thư viện</a></li>
+								<li><a href="">TT Ngoại Ngữ</a></li>
+								<li><a href="">TT Tin Học NTT</a></li>
+								<li><a href="">TT QH Doanh Nghiệp & Khởi nghiệp</a></li>
+								<li><a href="">TT Đào tạo theo nhu cầu XH</a></li>
+								<li><a href="">TT Xuất khẩu lao động Texgamex</a></li>
+								<li><a href="">Công đoàn Trường ĐH Nguyễn Tất Thành</a></li>
+							</ul>
+						</div>
+						<div class="col-md-6 col-lg-3 sociaty">
+				  			<ul>
+				  				<li><a href=""><i class="fab fa-facebook-square"></i>Facebook</a></li>
+				  				<li><a href=""><i class="fab fa-google-plus-g"></i>Google Plus</a></li>
+				  				<li><a href=""><i class="fab fa-youtube"></i>Youtube</a></li>
+				  				<li><a href=""><i class="fas fa-envelope"></i>E-mail</a></li>
+				  				<li><a href=""><i class="fab fa-youtube"></i>Youtube</a></li>
+				  				<li><a href=""><i class="fa fa-wechat"></i>Zalo</a></li>
+				  				<li><a href=""><i class="fa fa-mortar-board"></i>E-office</a></li>
+				  				<li><a href=""><i class="fa fa-users"></i>Tuyển dụng</a></li>
+				  			</ul>
+				  		</div>
+					</div>
+				</div>
+			</div>
+				
+			<div class="footer1">
+				<div class="wrapper">
+					<div class="row">
+						<div class="col-md-6">
+							<h4>THÔNG TIN LIÊN HỆ</h4>
+							<p>Trụ sở chính:<b>300A – Nguyễn Tất Thành, Phường 13, Quận 4, TP. Hồ Chí Minh, Việt Nam</b></p>
+							<p>Điện thoại:<b>1900 2039</b>Fax:<b>028 39 404 759</b></p>
+							<p>Hotline:<b>0902 298 300 – 0906 298 300 – 0912 298 300 – 0914 298 300</b></p>
+							<p>Email:<b>tttvtsinh@ntt.edu.vn – bangiamhieu@ntt.edu.vn</b></p>
+						</div>
+						<div class="col-md-6">
+							<p>©2017. Bản quyền thuộc về trường <b>Đại học Nguyễn Tất Thành</b></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="wrapper_chatbox">
+				<div class="chatbox" >
+					<button href="#">Hãy để lại lời nhắn</button>
+				</div>
+			<div class="content_chatbox">
+				<div class="top">
+					<div class="d-flex row">
+						<div class="col-8 d-flex justify-content-start">
+							<p>Hãy để lại lời nhắn</p>
+						</div>
+						<div class="col-4 d-flex justify-content-end">
+							<i id="close" class="fas fa-times-circle"></i>
+						</div>
+					</div>
+					<div>Bạn vui lòng điền vào biểu mẫu dưới đây và chúng tôi sẽ liên hệ lại với bạn ngay khi có thể.</div>
+				</div>
+				<div class="main">
+					<input type="text" placeholder="*Tên" name="">
+					<input type="text" placeholder="*E-mail" name="">
+					<input type="text" placeholder="Điện thoại" name="">
+					<input type="text" placeholder="Ngày sinh" name="">
+					<textarea placeholder="*lời nhắn"></textarea>
+					<input type="submit" value="Hoàn tất" name="">
+					<div class="tiny">
+						We're <img src="https://cdn.jsdelivr.net/emojione/assets/png/26a1.png?v=2.2.7">  by tawk.to
+					</div>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
--- project/index.php
+++ project/index.php
@@ -10,7 +10,7 @@
 	<!--boostrap 4-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<!--css-->
-	<link rel="stylesheet" media="screen" type="text/css" href="css/index.css">
+	<link rel="stylesheet" media="screen" type="text/css" href="css/style.css">
 	<!--icon-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
@@ -87,7 +87,7 @@
 												<a href="#"><i class="fa fa-angle-right"></i>ĐH Nguyễn Tất Thành – 20 năm dấu ấn một chặng đường</a>
 											</li>
 											<li>
-												<a href="#"><i class="fa fa-angle-right"></i>NỎ THẦN - TẬP 8 | BẬT MÍ VỀ NỎ THẦN VÀ NHỮNG CÂU CHUYỆN CÓ THẬTg</a>
+												<a href="#"><i class="fa fa-angle-right"></i>NỎ THẦN - TẬP 8 | BẬT MÍ VỀ NỎ THẦN VÀ NHỮNG CÂU CHUYỆN CÓ THẬT</a>
 											</li>
 										</ul>
 									</div>
@@ -112,12 +112,10 @@
 						<div class="title">
 							<b>TẠI SAO CHỌN ĐẠI HỌC NGUYỄN TẤT THÀNH?</b>
 							<div class="d-flex justify-content-center">
-								<div class="ngang">
-								
-								</div>
+								<hr>
 							</div>
-							
 						</div>
+						<!-- nội dung bên trong -->
 						<div class="content">
 							<div class="carousel_wrapper_track">
 								<section class="center slider">
@@ -127,15 +125,14 @@
 								      	<h5>Chuẩn 3 sao QS-Starts</h5>
 								      	<p>Xếp hạng quốc tế 3 sao theo chuẩn QS-Stars, một trong các chuẩn xếp hạng hàng đầu dành cho các trường đại học trên thế giới.ếp hạng quốc tế 3 sao theo chuẩn QS-Stars, một trong các chuẩn xếp hạng hàng đầu dành cho các trường đại học trên thế giới.ếp hạng quốc tế 3 sao theo chuẩn QS-Stars, một trong các chuẩn xếp hạng hàng đầu dành cho các trường đại học trên thế giới.	</p>
 								      </div>
-								      
 								    </div>
+
 								    <div class="item">
 								      <img src="http://ntt.edu.vn/web/upload/images/hp_xhoi-01.svg">
 								      <div class="main">
 								      	<h5>Đạt chuẩn chất lượng quốc gia</h5>
 								      	<p>Xếp hạng quốc tế 3 sao theo chuẩn QS-Stars, một trong các chuẩn xếp hạng hàng đầu dành cho các trường đại học trên thế giới.</p>
 								      </div>
-								      
 								    </div>
 
 								    <div class="item">
@@ -144,7 +141,6 @@
 								      	<h5>Đại học hạnh phúc</h5>
 								      	<p>Xếp hạng quốc tế 3 sao theo chuẩn QS-Stars, một trong các chuẩn xếp hạng hàng đầu dành cho các trường đại học trên thế giới.</p>
 								      </div>
-								      
 								    </div>
 
 								    <div class="item">
@@ -153,7 +149,6 @@
 								      	<h5>Top 10 thương hiệu nổi tiếng</h5>
 								      	<p>Xếp hạng quốc tế 3 sao theo chuẩn QS-Stars, một trong các chuẩn xếp hạng hàng đầu dành cho các trường đại học trên thế giới.</p>
 								      </div>
-								      
 								    </div>
 
 								    <div class="item">
@@ -162,28 +157,28 @@
 								      	<h5>95% sinh viên tốt nghiệp có việc làm</h5>
 								      	<p>Xếp hạng quốc tế 3 sao theo chuẩn QS-Stars, một trong các chuẩn xếp hạng hàng đầu dành cho các trường đại học trên thế giới.</p>
 								      </div>
-								      
 								    </div>
-							    
-								  </section>
-								
+								</section>
 							</div>
 						</div>
 					</div>
-					
 				</div>
 			</div>
-
-			<div class="tintuc cooperate">
+			<hr style="margin-right: 8%; margin-left: 8%;">
+			
+			<!-- phần hợp tác quốc tế, môi trường học tập và sự kiện nổi bật -->
+			<div class="tintuc_moitruong_sukien">
 				<div class="wrapper">
 					<div class="row">
 						<div class="col-md-4">
 							<h4 class="title">Hợp tác quốc tế </h4>
-							<div class="ngang"></div>
+							<hr style="height: 1px; background-color: #aa914d;width: 35px; margin-top: -1%; margin-left: -0.5%;">
 							<div class="row content">
 								<div class="col-12">
-									<img src="http://ntt.edu.vn/web/upload/images/Tin_Tuc/201909_TIN/DSC04631.JPG" class="w-100">
-									<a class="" href="">Khoa Công nghệ Sinh học ĐH Nguyễn Tất Thành làm việc với ĐH Hiroshima</a>
+									<a href="">
+										<img src="http://ntt.edu.vn/web/upload/images/Tin_Tuc/201909_TIN/DSC04631.JPG" class="w-100">
+									</a>
+									<a class="content1" href="">Khoa Công nghệ Sinh học ĐH Nguyễn Tất Thành làm việc với ĐH Hiroshima</a>
 									<p>NTTU - Sáng ngày 17/09/2019, khoa Công nghệ Sinh học Trường ĐH Nguyễn Tất Thành đã có buổi làm việc với ĐH Hiroshima (Nhật Bản)</p>
 									<div class="xemthem">
 										<a href="">
@@ -192,17 +187,18 @@
 										</a>
 									</div>
 								</div>
-								
 							</div>
 						</div>
+
 						<div class="col-md-4">
-							<h4 class="title">Môi trường học tập</h4Môi trường học tập>
-							<div class="ngang"></div>
+							<h4 class="title">Môi trường học tập</h4>
+							<hr style="height: 1px; background-color: #aa914d;width: 35px; margin-top: -1%; margin-left: -0.5%;">
 							<div class="row content">
-						
 								<div class="col-md-12">
-									<img src="http://ntt.edu.vn/web/upload/images/3e3.JPG" class="w-100">
-									<a class="" href="">Tìm hiểu về các câu lạc bộ tại Trường ĐH Nguyễn Tất Thành</a>
+									<a href="">
+										<img src="http://ntt.edu.vn/web/upload/images/3e3.JPG" class="w-100">
+									</a>
+									<a class="content1" href="">Tìm hiểu về các câu lạc bộ tại Trường ĐH Nguyễn Tất Thành</a>
 									<p>NTTU - Những năm gần đây, bên cạnh việc nâng cao chất lượng đào tạo, Trường ĐH Nguyễn Tất Thành không ngừng phát triển mô hình câu lạc bộ sinh viên nhằm tạo môi trường học tập và vui chơi tốt nhất cho người học. Thông qua mô hình CLB, các bạn sinh viên có thể dễ dàng trao đổi kiến</p>
 									<div class="xemthem">
 										<a href="">
@@ -213,11 +209,11 @@
 								</div>
 							</div>
 						</div>
+
 						<div class="col-md-4">
 							<h4 class="title">Sự kiện nổi bật</h4>
-							<div class="ngang"></div>
+							<hr style="height: 1px; background-color: #aa914d;width: 35px; margin-top: -1%; margin-left: -0.5%;">
 							<div class="row content">
-						
 								<div class="col-md-12 noibat">
 									<div class="row item_noibat">
 										<div class="col-3 col-md-3 day">
@@ -232,7 +228,8 @@
 													07:30
 												</li>
 												<li class="line-2">
-													Hội trường A.801, 300A Nguyễn Tất Thành, phường 13, quận 4, TP. HCM
+													| Hội trường A.801, 300A Nguyễn Tất Thành, phường 13, quận 4, TP. HCM
+													<hr>
 												</li>
 											</ul>
 										</div>
@@ -251,7 +248,8 @@
 													07:30
 												</li>
 												<li class="line-2">
-													Hội trường A.801, 300A Nguyễn Tất Thành, phường 13, quận 4, TP. HCM
+													| Hội trường A.801, 300A Nguyễn Tất Thành, phường 13, quận 4, TP. HCM
+													<hr>
 												</li>
 											</ul>
 										</div>
@@ -270,120 +268,30 @@
 													07:30
 												</li>
 												<li class="line-2">
-													Hội trường A.801, 300A Nguyễn Tất Thành, phường 13, quận 4, TP. HCM
+													| Hội trường A.801, 300A Nguyễn Tất Thành, phường 13, quận 4, TP. HCM
+													<hr>
 												</li>
 											</ul>
 										</div>
+										<div class="xemthem">
+											<a href="" style="margin: 0 !important;font-size: 13px !important;color: #024282;font-weight: bolder;padding-top: -15%;">
+												<i class="fa fa-chevron-circle-right mr-2 c-blue-a5"></i>
+												Xem thêm 
+											</a>
+										</div>
 									</div>
-
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-
-				<div class="contact">
-					<div class="wrapper">
-						<div class="row ">
-							<div class="col-md-6 col-lg-3">
-								<h4><a href="">ĐH NGUYỄN TẤT THÀNH</a></h4>
-								<ul>
-									<li><a href="">Thư ngỏ</a></li>
-									<li><a href="">Tầm nhìn - Sứ mạng</a></li>
-									<li><a href="">Văn hóa ĐH Nguyễn Tất Thành</a></li>
-									<li><a href="">Thông tin 3 công khai</a></li>
-									<li><a href="">Đảm bảo chất lượng</a></li>
-									<li><a href="">Phát triển bền vững</a></li>
-									<li><a href="">Trường Trung cấp Nguyễn Tất Thành</a></li>
-									<li><a href="">Trường Tiểu học Anh Việt Mỹ</a></li>
-								</ul>
-							</div>
-							<div class="col-md-6 col-lg-3 cot-2">
-								<h4><a href="">PHÒNG - BAN</a></h4>
-								<h4><a href="">THƯ VIỆN ẢNH</a></h4>
-								<h4><a href="">E-LEARNING</a></h4>
-							</div>
-							<div class="col-md-6 col-lg-3">
-								<h4><a href="">TRUNG TÂM - VIỆN</a></h4>
-								<ul>
-									<li><a href="">Viện Đào Tạo Quốc Tế</a></li>
-									<li><a href="">Viện Công Nghệ Cao</a></li>
-									<li><a href="">Viện Đào tạo Sau đại học</a></li>
-									<li><a href="">Trung tâm Thông tin Thư viện</a></li>
-									<li><a href="">TT Ngoại Ngữ</a></li>
-									<li><a href="">TT Tin Học NTT</a></li>
-									<li><a href="">TT QH Doanh Nghiệp & Khởi nghiệp</a></li>
-									<li><a href="">TT Đào tạo theo nhu cầu XH</a></li>
-									<li><a href="">TT Xuất khẩu lao động Texgamex</a></li>
-									<li><a href="">Công đoàn Trường ĐH Nguyễn Tất Thành</a></li>
-								</ul>
-							</div>
-							<div class="col-md-6 col-lg-3 sociaty">
-				  				<ul>
-				  					<li><a href=""><i class="fab fa-facebook-square"></i>Facebook</a></li>
-				  					<li><a href=""><i class="fab fa-google-plus-g"></i>Google Plus</a></li>
-				  					<li><a href=""><i class="fab fa-youtube"></i>Youtube</a></li>
-				  					<li><a href=""><i class="fas fa-envelope"></i>E-mail</a></li>
-				  					<li><a href=""><i class="fab fa-youtube"></i>Youtube</a></li>
-				  					<li><a href=""><i class="fa fa-wechat"></i>Zalo</a></li>
-				  					<li><a href=""><i class="fa fa-mortar-board"></i>E-office</a></li>
-				  					<li><a href=""><i class="fa fa-users"></i>Tuyển dụng</a></li>
-				  				</ul>
-				  			</div>
-						</div>
-					</div>
-				</div>
-				
-				<div class="footer">
-					<div class="wrapper">
-						<div class="row">
-							<div class="col-md-6">
-								<h4>THÔNG TIN LIÊN HỆ</h4>
-								<p>Trụ sở chính:<b>300A – Nguyễn Tất Thành, Phường 13, Quận 4, TP. Hồ Chí Minh, Việt Nam</b></p>
-								<p>Điện thoại:<b>1900 2039</b>Fax:<b>028 39 404 759</b></p>
-								<p>Hotline:<b>0902 298 300 – 0906 298 300 – 0912 298 300 – 0914 298 300</b></p>
-								<p>Email:<b>tttvtsinh@ntt.edu.vn – bangiamhieu@ntt.edu.vn</b></p>
-								
-							</div>
-							<div class="col-md-6">
-								<p>©2017. Bản quyền thuộc về trường <b>Đại học Nguyễn Tất Thành</b></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			<div class="wrapper_chatbox">
-				<div class="chatbox" >
-					<button href="#">Hãy để lại lời nhắn</button>
-
-				</div>
-				<div class="content_chatbox">
-					<div class="top">
-						<div class="d-flex row">
-							<div class="col-8 d-flex justify-content-start">
-								<p>Hãy để lại lời nhắn</p>
-							</div>
-							<div class="col-4 d-flex justify-content-end">
-								<i id="close" class="fas fa-times-circle"></i>
-							</div>
-							
-						</div>
-						
-						<div>Bạn vui lòng điền vào biểu mẫu dưới đây và chúng tôi sẽ liên hệ lại với bạn ngay khi có thể.</div>
-					</div>
-					<div class="main">
-						<input type="text" placeholder="*Tên" name="">
-						<input type="text" placeholder="*E-mail" name="">
-						<input type="text" placeholder="Điện thoại" name="">
-						<input type="text" placeholder="Ngày sinh" name="">
-						<textarea placeholder="*lời nhắn"></textarea>
-						<input type="submit" value="Hoàn tất" name="">
-						<div class="tiny">
-							We're <img src="https://cdn.jsdelivr.net/emojione/assets/png/26a1.png?v=2.2.7">  by tawk.to
-						</div>
-					</div>
-				</div>
-			</div>
+			<br>
+			
+			<!-- phần footer -->
+			<?php  
+				include"footer.php"
+			?>
 
 	</div>
 	<!--Start of Tawk.to Script-->
